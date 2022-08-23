4475de5e604851b5ba6bbc6c004655fb447bad2a
William-886 <Kingliamm93@gmail.com>
2022-08-23 15:14:30 +0000

    00 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for a in range(0, 100):
-    if a < 10:
+    if a < 99:
         print("{:02d}, ".format(a), end="")
-    elif a > 9 and a < 99:
-        print("{:d}, ".format(a), end="")
     else:
         print(a)
