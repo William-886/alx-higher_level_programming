688814eb37ce0f9823018cb139e3945e223107db
William-886 <Kingliamm93@gmail.com>
2022-08-27 16:02:55 +0000

    Hot fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    # my_string = ' '.join(map(str, sentence))
     length = len(sentence)
     first_c = sentence[0]
-    if length <= 1:
-        first_c = 'None'
+    if sentence == "":
+        length = 0
+        first_c = None
     return (length, first_c)
