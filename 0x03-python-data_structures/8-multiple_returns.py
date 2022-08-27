ed36b6e868bcc6bd46faaa12c60d9ed8ac902ae6
William-886 <Kingliamm93@gmail.com>
2022-08-27 16:05:23 +0000

    Hot fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,5 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_c = sentence[0]
     if sentence == "":
-        length = 0
-        first_c = None
+        return (0, None)
     return (length, first_c)
