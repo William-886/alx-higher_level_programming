df70ceab7a09d1bf899c3fe8bcfd48c9965f549a
William-886 <Kingliamm93@gmail.com>
2022-08-27 14:45:51 +0000

    Hot fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,11 @@
 def print_matrix_integer(matrix=[[]]):
     for item in matrix:
-        my_string = ' '.join(map(str, item))
-        print("{}".format(my_string))
+        a = 0
+        b = len(item) - 1
+        for c in item:
+            if a < b:
+                print("{} ".format(c), end="")
+            else:
+                print("{}".format(c), end="")
+            a += 1
+        print()
