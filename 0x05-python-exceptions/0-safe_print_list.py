af8a749c760ecfe097d230bf6119f7ffeb564df3
William-886 <Kingliamm93@gmail.com>
2022-09-12 12:22:56 +0000

    New commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for a in range(x):
         try:
-            print("{}".format(my_list[a]), end="")
+            print("{}".format(my_list[a]), end='')
         except:
             break
         else:
