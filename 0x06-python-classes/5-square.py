785c6cd1158e35538bb8ce4d4fc69b3e10fa6713
William-886 <Kingliamm93@gmail.com>
2022-09-13 20:34:09 +0000

    All done

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -19,7 +19,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
